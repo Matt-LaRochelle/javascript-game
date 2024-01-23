52ab3e5c75c592c925891565ff347d5adeea76cb
matthew <larochelle.matthew@gmail.com>
2024-01-22 16:55:49 -0800

    enemy movement 3

--- movement/script.js
+++ movement/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 23;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -11,11 +11,11 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = './enemies/enemy2.png';
+        this.image.src = './enemies/enemy3.png';
 
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
@@ -23,12 +23,21 @@ class Enemy {
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 7;
+        this.angleSpeed = Math.random() * 2 + 0.5;
+        this.curve = Math.random() * 200 + 50;
     }
     update() {
-        this.x -= this.speed;
-        this.y += this.curve * Math.sin(this.angle);
+        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI / 180) + (canvas.width/2 - this.width/2);
+        this.y = canvas.height/2 * Math.cos   (this.angle * Math.PI / 180) + (canvas.height/2 - this.height/2);   
+        
+        // Different patterns by changing Math.PI / #
+        // this.x = canvas.width/2 * Math.sin(this.angle * Math.PI / 45) + (canvas.width/2 - this.width/2);
+        // this.y = canvas.height/2 * Math.cos   (this.angle * Math.PI / 180) + (canvas.height/2 - this.height/2); 
+
+        // this.x = canvas.width/2 * Math.sin(this.angle * Math.PI / 500) + (canvas.width/2 - this.width/2);
+        // this.y = canvas.height/2 * Math.cos   (this.angle * Math.PI / 100) + (canvas.height/2 - this.height/2);   
+        
+        
         this.angle += this.angleSpeed;
         if (this.x + this.width < 0) this.x = canvas.width;
         // animate sprites
